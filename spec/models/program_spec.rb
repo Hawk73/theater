ef6cd2d43366b8c98e931340c1198895d4398acd
m.bagautdinov <m.bagautdinov@fun-box.ru>
2020-01-12 22:52:30 +0400

    Fix 1 day crossing bug.

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Program, type: :model do
+  shared_examples 'model with crossing period checking' do
+    describe 'checking in model' do
+      it 'adds error' do
+        subject.validate
+        expect(subject.errors.messages[:activity_period]).to eq(["can't be crossed with another"])
+      end
+    end
+
+    describe 'checking in DB' do
+      it 'raises error' do
+        expect { subject.save(validate: false) }.to raise_error(ActiveRecord::StatementInvalid)
+      end
+    end
+  end
+
   describe 'validation of title' do
     subject { build(:program) }
 
@@ -16,7 +31,7 @@ RSpec.describe Program, type: :model do
 
   describe 'validation of activity_period' do
     let(:start_date) { Date.new(2019, 1, 1) }
-    let(:end_date) { Date.new(2019, 1, 30) }
+    let(:end_date) { Date.new(2019, 1, 10) }
 
     subject { build(:program, activity_period: start_date..end_date) }
 
@@ -30,23 +45,26 @@ RSpec.describe Program, type: :model do
     end
 
     context 'another program for the same dates exists' do
-      before { create(:program, activity_period: (end_date - 1.day)..(end_date + 1.day)) }
+      context '2 days crossing' do
+        before { create(:program, activity_period: (end_date - 1.day)..(end_date + 1.day)) }
 
-      describe 'checking in model' do
-        it 'adds error' do
-          subject.validate
-          expect(subject.errors.messages[:activity_period]).to eq(["can't be crossed with another"])
-        end
+        it_behaves_like 'model with crossing period checking'
       end
 
-      describe 'checking in DB' do
-        it 'raises error' do
-          expect { subject.save(validate: false) }.to raise_error(ActiveRecord::StatementInvalid)
-        end
+      context '1 day crossing at the beginning' do
+        before { create(:program, activity_period: start_date..start_date) }
+
+        it_behaves_like 'model with crossing period checking'
+      end
+
+      context '1 day crossing at the end' do
+        before { create(:program, activity_period: end_date..end_date) }
+
+        it_behaves_like 'model with crossing period checking'
       end
     end
 
-    context 'another program has different period' do
+    context 'another program with different period exists' do
       before { create(:program, activity_period: (end_date + 1.day)..(end_date + 2.day)) }
 
       it 'does not add error' do
